6fad6692367212f508306f27ca8bb2b8b58d6430
Dmitry Zhilin <dmitry.zhilin@list.ru>
2024-03-20 18:00:40 +0700

    floating-link

--- script/script.js
+++ script/script.js
@@ -41,3 +41,14 @@ next.addEventListener(`click`, ()=> {
    plusSlides(++slideIndex)
 })
 });
+
+
+
+
+// $(window).on("scroll", function() {
+//    // Если высота больше 200px 
+//    // Добавляем классу header класс fixed
+//      if ($(window).scrollTop() > 200) $('.header').addClass('fixed');
+//    // Иначе удаляем класс fixed
+//            else $('.header').removeClass('fixed');
+//      });
